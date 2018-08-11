ea0a4df056a297bbded369595d2c8a8a97ef2bd9
Joshua Comish <jcomish@sourceiron.com>
2018-08-10 23:59:40 -0500

    JC - Cleaner multiple execution on page objects

--- Scenario.py
+++ Scenario.py
@@ -40,26 +40,52 @@ class Scenario:
     def import_method_string(self, module):
         return "from Fitv.FitActions." + module + " import " + module
 
-    def write_page_object(self, path):
-        output_string = ""
+    def get_locator_index(self, lines):
+        locator_index = 0
+        for line in lines:
+            if "def" in line:
+                return locator_index - 1
+            locator_index += 1
+
+    def create_fresh_page_object(self, path):
         modules = set([])
         for step in self.steps:
             modules.add(FITV_METHOD_MAP[step.input_type][2])
-        if not path.exists():
-            output_string += ("from Fitv.FitPage import FitPage\n" +
-                              "\n".join([self.import_method_string(module) for module in modules]) +
-                              "\n\n\nclass " + self.feature_name + "PO(FitPage):\n" +
-                              "    Url = \"" + self.url + " \"\n\n")
+        output_string = ""
+        output_string += ("from Fitv.FitPage import FitPage\n" +
+                          "\n".join([self.import_method_string(module) for module in modules]) +
+                          "\n\n\nclass " + self.feature_name + "PO(FitPage):\n" +
+                          "    Url = \"" + self.url + " \"\n\n")
 
-        if not path.exists():
-            output_string += ("".join([step.write_page_object_locator(path) for step in self.steps]) + "\n")
-            output_string += ("".join([step.write_page_object_method(path) for step in self.steps]))
-        else:
-            output_string += ("".join([step.write_page_object_locator(path) for step in self.steps]))
-            output_string += ("".join([step.write_page_object_method(path) for step in self.steps]))
-
+        output_string += ("".join([step.write_page_object_locator(path) for step in self.steps]) + "\n")
+        output_string += ("".join([step.write_page_object_method(path) for step in self.steps]))
         p = open(path, 'a')
         p.write(output_string)
+
+    def write_page_object(self, path):
+        if not path.exists():
+            self.create_fresh_page_object(path)
+
+        else:
+            locators = ("".join([step.write_page_object_locator(path) for step in self.steps]))
+            methods = ("".join([step.write_page_object_method(path) for step in self.steps]))
+
+            output = open(path, 'r')
+            output = output.readlines()
+            locator_index = self.get_locator_index(output)
+            if not locator_index:
+                self.create_fresh_page_object(path)
+                return
+            for locator in locators:
+                output.insert(locator_index, locator)
+                locator_index += 1
+
+            output.append("".join(methods))
+
+            p = open(path, 'w')
+            p.write("".join(output))
+
+
 
 
     def write_steps(self, path):
